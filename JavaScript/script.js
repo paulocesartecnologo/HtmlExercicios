74acc579bfe8a2e2815794b4b5a0c9242251b732
Paulo Reis <paulocesartecnologo@gmail.com>
2021-02-25 00:18:02 -0300

    add

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,29 @@
-alert("Funcionando");
+/*--------------SAIDAS / OUTPUT -------------------------------------------------------
+document.getElementById("titulo").innerHTML="Opa, tudo bem ?";
+document.write("Algum coisa");
+window.alert("Mensagem de exemplo")
+console.log("A mensagem que eu irei inserir aqui sera mostrado no console")
+*/
+/*-------------- VARIAVEIS ------------------------------------------------------------
+var nome  = "Cesar";
+let nome  = "Cesar";                        //Hoisting / variavel local
+const nome= "Cesar";                        //Hoisting / variavel de valor constante
+var sobrenome = "Reis";                     //Hoisting / variavel global
+var idade = 90;
+var a = 10;
+var b = 15;
+var total = a + b; 
+var nomecompleto = nome + " " + sobrenome;
+//document.write(nomecompleto);
+*/
+/*-------------- CONDICIONAL ------------------------------------------------------------
+var hora = 20;
+    if(hora <= 12){
+        window.alert("Bom Dia");
+    }   else if(hora <= 18){
+            window.alert("Boa Tarde");
+        }else if (hora <= 23){
+            window.alert("Boa Noite");   
+        }
+*/
+//-------------- CONDICIONAL ------------------------------------------------------------
