5c7dabb1cdf86771676db8729a8a8f4858509c7c
Paulo Reis <paulocesartecnologo@gmail.com>
2021-02-26 00:28:50 -0300

    add

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -5,10 +5,11 @@ window.alert("Mensagem de exemplo")
 console.log("A mensagem que eu irei inserir aqui sera mostrado no console")
 */
 /*-------------- VARIAVEIS ------------------------------------------------------------
-var nome  = "Cesar";
+var nome  = "Cesar";                        //Hoisting / variavel global
 let nome  = "Cesar";                        //Hoisting / variavel local
 const nome= "Cesar";                        //Hoisting / variavel de valor constante
 var sobrenome = "Reis";                     //Hoisting / variavel global
+var salvo   = true;                         //variavel booleana
 var idade = 90;
 var a = 10;
 var b = 15;
@@ -22,8 +23,52 @@ var hora = 20;
         window.alert("Bom Dia");
     }   else if(hora <= 18){
             window.alert("Boa Tarde");
-        }else if (hora <= 23){
+        }elseif (hora <= 23){
             window.alert("Boa Noite");   
         }
 */
-//-------------- CONDICIONAL ------------------------------------------------------------
+/*-------------- GetElementById InnerHTML------------------------------------------------
+//document.getElementById("titulo").innerHTML = "Olá Mundo";
+var titulo = document.getElementById("titulo");
+titulo.innerHTML = "Segundo Titulo";
+var campo = document.getElementById("campo");
+campo.value = "Reis";
+*/
+/*-------------- Funções ----------------------------------------------------------------
+function alterar(){
+    document.getElementById("titulo").innerHTML = titulo;
+    document.getElementById("campo").value = titulo;    
+}//alterar();
+
+function somar(x,y){
+    let total = x + y;
+    return total;
+}
+var resultado = somar(50,50);
+console.log(resultado);
+*/
+//-------------- Evento de Clique ----------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
