8129abad0bf0c61fdd993f993497e468072d58f2
Paulo Reis <paulocesartecnologo@gmail.com>
2021-02-26 21:03:50 -0300

    add new line

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -50,12 +50,36 @@ console.log(resultado);
 //-------------- Evento de Clique ----------------------------------------------------------------
 function clicou(){
     alert("Você clicou no botão!");
-    
 }
+function digitou(e){
+    //console.log(e);
+    if(e.KeyCode == 13 ){
+        let texto = document.getElementById("campo").value;
+        console.log(texto);
+    }
+}
+function azul(){
+    limpar();
+    document.getElementById("titulo").classList.add('azul');
+}
+function vermelho(){
+    limpar();
+    document.getElementById('titulo').classList.add('vermelho');
+}
+function verde(){
+    limpar();
+    document.getElementById('titulo').classList.add('verde');
+}
+function limpar(){
+    document.getElementById('titulo').classList.remove("azul");
+    document.getElementById('titulo').classList.remove("vermelho");
+    document.getElementById('titulo').classList.remove("verde");
+}
+function mostrarTelefone(elemento){
+    elemento.style.display='none'
+    document.getElementById("telefone").style.display="block";
 
-
-
-
+}
 
 
 
